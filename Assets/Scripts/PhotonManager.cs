5c62b9d022643fa75b6149efaeebc0c7f6635472
Thomas <thomasreuvers@upcmail.nl>
2019-05-19 22:29:29 +0200

    Upload Game

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour
+{
+    [SerializeField]private GameObject player;
+    [SerializeField]private GameObject lobbyCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("1.0");  
+    }
+
+    //Look for room "Room" if it exists execute OnJoinedRoom if it does not exists create a new "Room"
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions() { MaxPlayers = 3 }, TypedLobby.Default);
+    }
+
+    //When Joined the room Execute this code
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", player.transform.position, Quaternion.identity, 0);
+        lobbyCamera.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed; //Horizontal speed
+    public float jumpForce; //Jump force of player
+    private float moveInput; //Move Inpute
+
+    public SpriteRenderer sr;
+    Rigidbody2D rb;
+
+    private bool isGrounded; //Either true or false
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Jump logic
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+ 
+        //Horizontal movements (left & right)
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2((moveInput * speed) * Time.deltaTime, rb.velocity.y);
+
+        if(moveInput > 0){
+            sr.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
